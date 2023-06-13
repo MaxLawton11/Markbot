2f85f2d599ae068dff7d00890bd9d16d57971624
Max Lawton <max@lawtonnation.com>
2023-06-12 20:49:04 -0400

    test

--- main.py
+++ main.py
@@ -5,20 +5,4 @@ if __name__ == '__main__' :
    while True :
       
       if test_pos != None:
-         # Access the desired variables
-         latitude = test_pos.latitude
-         longitude = test_pos.longitude
-         latitude_error = test_pos.latitude_error
-         longitude_error = test_pos.longitude_error
-         speed = test_pos.speed
-         altitude = test_pos.altitude
-         altitude_error = test_pos.altitude_error
-
-         # Use the variables as needed
-         print("Latitude:", latitude)
-         print("Longitude:", longitude)
-         print("Latitude Error:", latitude_error)
-         print("Longitude Error:", longitude_error)
-         print("Speed:", speed)
-         print("Altitude:", altitude)
-         print("Altitude Error:", altitude_error)
+         print(test_pos)
