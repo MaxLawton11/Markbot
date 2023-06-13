97e18df5efe14bacd9c4b1e69baab7c08b3506f6
Max Lawton <max@lawtonnation.com>
2023-06-12 20:25:18 -0400

    new subpross

--- testGPS.py
+++ testGPS.py
@@ -1,28 +1,7 @@
-import gps
+import subprocess
 
-print(1)
+# Run cgps command and capture output
+output = subprocess.check_output(["cgps", "-s"]).decode("utf-8")
 
-# Connect to the GPSd service
-session = gps.gps("localhost", "2947")
-session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
-
-print(2)
-
-# Main loop
-while True:
-    try:
-        # Wait for new data
-        report = session.next()
-        if report['class'] == 'TPV':
-            if hasattr(report, 'lat') and hasattr(report, 'lon'):
-                print("Latitude:", report.lat)
-                print("Longitude:", report.lon)
-    except KeyboardInterrupt:
-        # Exit gracefully on Ctrl+C
-        session.close()
-        break
-    except StopIteration:
-        # End of file reached
-        session.close()
-        print("GPSD has terminated")
-        break
+# Print the raw data
+print(output)
