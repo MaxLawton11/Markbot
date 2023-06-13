0b61d0d9c82b5f84ef065c32e5401baf2f19c2a2
Max Lawton <max@lawtonnation.com>
2023-06-12 20:35:21 -0400

    back to gps

--- testGPS.py
+++ testGPS.py
@@ -1,26 +1,25 @@
-import serial
+from gps import gps, WATCH_ENABLE
 
-# Configure the serial port
-port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=1)
+# Create a GPS object
+session = gps(mode=WATCH_ENABLE)
 
 # Main loop
 while True:
     try:
-        # Read a line of data from the serial port
-        line = port.readline().decode("utf-8")
+        # Wait for new data
+        report = session.next()
         
-        # Process the GPS data
-        if line.startswith("$GPRMC"):
-            # Assuming the data follows the NMEA format (e.g., $GPRMC)
-            data = line.split(",")
-            if len(data) >= 4:
-                latitude = data[3]
-                longitude = data[5]
-                
-                print("Latitude:", latitude)
-                print("Longitude:", longitude)
-    
+        # Check if the report is a TPV (Time-Position-Velocity) report
+        if report['class'] == 'TPV':
+            # Print the raw data
+            print(report)
+            
     except KeyboardInterrupt:
         # Exit gracefully on Ctrl+C
-        port.close()
+        session.close()
+        break
+    except StopIteration:
+        # End of file reached
+        session.close()
+        print("GPSD has terminated")
         break
