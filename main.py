c8c99b314650feb70b643919c0dab6661dd8cee0
Max Lawton <max@lawtonnation.com>
2023-06-12 20:44:51 -0400

    new vars

--- main.py
+++ main.py
@@ -3,4 +3,22 @@ from sensors import heading, position
 if __name__ == '__main__' :
    test_pos = position.GPS()
    while True :
-      print(test_pos.getPosition())
+      
+      if test_pos:
+         # Access the desired variables
+         latitude = test_pos.latitude
+         longitude = test_pos.longitude
+         latitude_error = test_pos.latitude_error
+         longitude_error = test_pos.longitude_error
+         speed = test_pos.speed
+         altitude = test_pos.altitude
+         altitude_error = test_pos.altitude_error
+
+         # Use the variables as needed
+         print("Latitude:", latitude)
+         print("Longitude:", longitude)
+         print("Latitude Error:", latitude_error)
+         print("Longitude Error:", longitude_error)
+         print("Speed:", speed)
+         print("Altitude:", altitude)
+         print("Altitude Error:", altitude_error)
--- sensors/position.py
+++ sensors/position.py
@@ -5,13 +5,31 @@ class GPS :
         # Connect to the GPSd service
         self.session = gps(mode=WATCH_ENABLE)
         
+        self.latitude = None
+        self.longitude = None
+        self.latitude_error = None
+        self.longitude_error = None
+        self.speed = None
+        self.altitude = None
+        self.altitude_error = None
+        
     def getPosition(self) :
         try:
             # Wait for new data
             report = self.session.next()
+        
             
             # Check if the report is a TPV (Time-Position-Velocity) report
             if report['class'] == 'TPV':
+                # Update the instance variables
+                self.latitude = report.get('lat')
+                self.longitude = report.get('lon')
+                self.latitude_error = report.get('epx')
+                self.longitude_error = report.get('epy')
+                self.speed = report.get('speed')
+                self.altitude = report.get('alt')
+                self.altitude_error = report.get('epv')
+                
                 # Return the raw data
                 return report
             
