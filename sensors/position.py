9b3312ad865ded187e4e17d337ac93cc6edbc2d8
Max Lawton <max@lawtonnation.com>
2023-06-12 20:38:56 -0400

    ok new class

--- sensors/position.py
+++ sensors/position.py
@@ -3,30 +3,23 @@ import gps
 class GPS :
     def __init__(self) :
         # Connect to the GPSd service
-        self.session = gps.gps("localhost", "2947")
-        self.session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+        self.session = gps(mode=WATCH_ENABLE)
         
     def getPosition(self) :
-        # loop until we get a result
-        while True:
-            try:
-                # wait for new data
-                report = self.session.next()
-                print(report)
-                if report['class'] == 'TPV':
-                    if hasattr(report, 'lat') and hasattr(report, 'lon'):
-                        latitude = report.lat
-                        longitude = report.lon
-                        return latitude, longitude
-            except KeyboardInterrupt:
-                # Exit gracefully on Ctrl+C
-                self.session.close()
-                break
-            except StopIteration:
-                # End of file reached
-                self.session.close()
-                print("GPSD has terminated")
-                break
+        try:
+            # Wait for new data
+            report = self.session.next()
+            
+            # Check if the report is a TPV (Time-Position-Velocity) report
+            if report['class'] == 'TPV':
+                # Return the raw data
+                return report
+            
+        except StopIteration:
+            # End of file reached
+            self.session.close()
+            print("GPSD has terminated")
+            return None
             
 class Tester :
     def __init__(self) :
