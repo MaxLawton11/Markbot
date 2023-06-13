b2353973d1bb700ae823c223a5ba8c5ead81acdc
Max Lawton <max@lawtonnation.com>
2023-06-12 20:25:51 -0400

    new sub

--- testGPS.py
+++ testGPS.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Run cgps command and capture output
-output = subprocess.check_output(["cgps", "-s"]).decode("utf-8")
+output = subprocess.check_output(["cgps"]).decode("utf-8")
 
 # Print the raw data
 print(output)
