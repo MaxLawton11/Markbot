58352a6d2bf68a264ba672d3c01ca8c2a471ab42
Max Lawton <max@lawtonnation.com>
2023-06-12 20:18:54 -0400

    new tester scrpit

---
+++ testGPS.py
@@ -0,0 +1,24 @@
+import gps
+
+# Connect to the GPSd service
+session = gps.gps("localhost", "2947")
+session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+
+# Main loop
+while True:
+    try:
+        # Wait for new data
+        report = session.next()
+        if report['class'] == 'TPV':
+            if hasattr(report, 'lat') and hasattr(report, 'lon'):
+                print("Latitude:", report.lat)
+                print("Longitude:", report.lon)
+    except KeyboardInterrupt:
+        # Exit gracefully on Ctrl+C
+        session.close()
+        break
+    except StopIteration:
+        # End of file reached
+        session.close()
+        print("GPSD has terminated")
+        break
