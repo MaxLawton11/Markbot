8cb33a0404c3ba12f35c9b7481e1c182fffae7ca
Max Lawton <max@lawtonnation.com>
2023-06-12 20:35:00 -0400

    now seral

--- testGPS.py
+++ testGPS.py
@@ -1,4 +1,26 @@
-import subprocess
-process = subprocess.Popen('cgps', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-out, err = process.communicate()
-print(out)
+import serial
+
+# Configure the serial port
+port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=1)
+
+# Main loop
+while True:
+    try:
+        # Read a line of data from the serial port
+        line = port.readline().decode("utf-8")
+        
+        # Process the GPS data
+        if line.startswith("$GPRMC"):
+            # Assuming the data follows the NMEA format (e.g., $GPRMC)
+            data = line.split(",")
+            if len(data) >= 4:
+                latitude = data[3]
+                longitude = data[5]
+                
+                print("Latitude:", latitude)
+                print("Longitude:", longitude)
+    
+    except KeyboardInterrupt:
+        # Exit gracefully on Ctrl+C
+        port.close()
+        break
