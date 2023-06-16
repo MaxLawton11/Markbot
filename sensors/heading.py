58bffb7c8dbdf71cde07107e573953a0d4681892
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-06-15 21:28:10 -0400

    Update heading.py

--- sensors/heading.py
+++ sensors/heading.py
@@ -3,7 +3,9 @@ class Compass :
         pass
     def getHeading(self) :
         pass
-    
+
+# look here: https://tutorials-raspberrypi.com/build-your-own-raspberry-pi-compass-hmc5883l/
+
 class Tester :
     def __init__(self) :
         print('we worken from heading')
