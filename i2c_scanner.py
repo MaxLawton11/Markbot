5172c1f6ec782cb3f00caf106776a1391b2cf89a
Max Lawton <max@lawtonnation.com>
2023-06-17 17:48:50 -0400

    new scanner

---
+++ i2c_scanner.py
@@ -0,0 +1,12 @@
+import smbus
+
+# Create I2C bus object
+bus = smbus.SMBus(1)  # Use 1 for Raspberry Pi Model B+
+
+# Scan for devices
+for address in range(0x03, 0x78):
+    try:
+        bus.read_byte(address)
+        print("Device found at address: 0x{:02X}".format(address))
+    except IOError:
+        pass
