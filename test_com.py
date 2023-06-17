a8dc6983360e06f12aa67b0e5fbd375acccf2034
Max Lawton <max@lawtonnation.com>
2023-06-17 19:06:27 -0400

    changed test method

--- test_com.py
+++ test_com.py
@@ -1,6 +1,4 @@
 import smbus
-import time
-import math
 
 # HMC5883L address
 address = 0x0D
@@ -9,42 +7,15 @@ address = 0x0D
 config_register = 0x00
 data_register = 0x03
 
-# Create I2C bus object for bus 3
-bus = smbus.SMBus(1)
-
-# Function to read signed 16-bit data from two consecutive registers
-def read_data(register):
-    high = bus.read_byte_data(address, register)
-    low = bus.read_byte_data(address, register + 1)
-    value = (high << 8) | low
-    if value > 32767:
-        value -= 65536
-    return value
+# Create I2C bus object
+bus = smbus.SMBus(1)  # Use 1 for Raspberry Pi Model B+
 
 # Set the configuration register
 bus.write_byte_data(address, config_register, 0x00)
 
 while True:
-    # Read magnetometer data
-    x = read_data(data_register)
-    z = read_data(data_register + 2)
-    y = read_data(data_register + 4)
+    # Check if data is available
+    data_available = bus.read_byte_data(address, data_register) & 0x01
 
-    # Print the raw values
-    print("Raw Magnetometer Data:")
-    print(f"X: {x}")
-    print(f"Y: {y}")
-    print(f"Z: {z}")
+    print(data_available)
 
-    # Calculate heading in degrees
-    heading = (180 * math.atan2(y, x)) / math.pi
-
-    # Normalize heading to a 0-360 degree range
-    if heading < 0:
-        heading += 360
-
-    # Print the heading
-    print("Heading: {0} degrees".format(heading))
-
-    # Wait for a while before reading again
-    time.sleep(0.5)
