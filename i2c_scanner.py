f277a8585d15743ee104a546412102f7d1f83248
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-06-22 20:04:09 -0400

    Update i2c_scanner.py

--- i2c_scanner.py
+++ i2c_scanner.py
@@ -10,3 +10,14 @@ for address in range(0x03, 0x78):
         print("Device found at address: 0x{:02X}".format(address))
     except IOError:
         pass
+
+# bus 3
+bus = smbus.SMBus(3)
+
+# Scan for devices
+for address in range(0x03, 0x78):
+    try:
+        bus.read_byte(address)
+        print("Device found at address: 0x{:02X}".format(address))
+    except IOError:
+        pass
