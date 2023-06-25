d3f830dac44103b14a17bb8f5295625a78b2c69b
Max Lawton <91839824+MaxLawton11@users.noreply.github.com>
2023-06-25 13:54:25 -0400

    Update i2c_scanner.py

--- i2c_scanner.py
+++ i2c_scanner.py
@@ -1,23 +1,16 @@
 import smbus
 
-# Create I2C bus object
-bus = smbus.SMBus(1)  # Use 1 for Raspberry Pi Model B+
 
-# Scan for devices
-for address in range(0x03, 0x78):
-    try:
-        bus.read_byte(address)
-        print("Device found at address: 0x{:02X}".format(address))
-    except IOError:
-        pass
+# loop through busses
+for i_bus in [1,3] :
+    
+    # Create I2C bus object
+    bus = smbus.SMBus(i_bus)  # Use 1 for Raspberry Pi Model B+
 
-# bus 3
-bus = smbus.SMBus(3)
-
-# Scan for devices
-for address in range(0x03, 0x78):
-    try:
-        bus.read_byte(address)
-        print("Device found at address: 0x{:02X}".format(address))
-    except IOError:
-        pass
+    # Scan for devices
+    for address in range(0x03, 0x78):
+        try:
+            bus.read_byte(address)
+            print("Device found at address: 0x{:02X} on Bus:{}".format(address, i_bus))
+        except IOError:
+            pass
